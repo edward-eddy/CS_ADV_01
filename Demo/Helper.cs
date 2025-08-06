0070977ec230e73216fe8428da17a6e206e193bf
edwar <edwardsameh99@gmail.com>
2025-08-06 23:55:19 +0300

    Vid01

---
+++ Demo/Helper.cs
@@ -0,0 +1,43 @@
+﻿namespace Demo
+{
+    // Generic Class
+    internal static class Helper/*<T>*/
+    {
+        // Generic SWAP Method
+        public static void SWAP<T>(ref T X, ref T Y)
+        {
+            T Temp = X;
+            X = Y;
+            Y = Temp;
+        }
+
+        public static void Print(string data)
+        {
+            Console.WriteLine(data);
+        }
+
+        #region Non Generic SWAP
+        //public static void SWAP(ref int X, ref int Y)
+        //{
+        //    int Temp = X;
+        //    X = Y;
+        //    Y = Temp;
+        //}
+
+        //public static void SWAP(ref double X, ref double Y)
+        //{
+        //    double Temp = X;
+        //    X = Y;
+        //    Y = Temp;
+        //}
+
+        //public static void SWAP(ref Point X, ref Point Y)
+        //{
+        //    Point Temp = X;
+        //    X = Y;
+        //    Y = Temp;
+        //} 
+        #endregion
+
+    }
+}
--- Demo/Program.cs
+++ Demo/Program.cs
@@ -4,7 +4,90 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Generics :
+            // C# Feature 2005 C# 2.0
+            // Before 2005 Class Object
+
+            // SWAP
+
+            //int A = 4, B = 5;
+
+            //Console.WriteLine($"A: {A}");
+            //Console.WriteLine($"B: {B}");
+
+            //Console.WriteLine(" ********* After SWAP ********* ");
+
+            //Helper.SWAP(ref A, ref B); // Passing By ref
+
+            //Console.WriteLine($"A: {A}");
+            //Console.WriteLine($"B: {B}");
+
+            //double L = 1.5, K = 2.6;
+
+            //Console.WriteLine($"L: {L}");
+            //Console.WriteLine($"K: {K}");
+            //Console.WriteLine(" ********* After SWAP ********* ");
+
+            //Helper.SWAP(ref L, ref K);
+
+            //Console.WriteLine($"L: {L}");
+            //Console.WriteLine($"K: {K}");
+
+            // Point : X, Y
+
+            //Point P01 = new Point(1,1);
+            //Point P02 = new Point(2,2);
+
+            //Console.WriteLine($"P01: {P01}");
+            //Console.WriteLine($"P02: {P02}");
+
+            //Console.WriteLine(" ********* After SWAP ********* ")
+
+            //Helper.SWAP(ref P01, ref P02);
+
+            //Console.WriteLine($"P01: {P01}");
+            //Console.WriteLine($"P02: {P02}");
+
+            // ==================================================================
+
+            int A = 4, B = 5;
+            double X = 1.5;
+
+            //Console.WriteLine($"A: {A}");
+            //Console.WriteLine($"B: {B}");
+
+            //Console.WriteLine(" ********* After SWAP ********* ");
+
+            //Helper.SWAP/*<int>*/(ref A, ref B); // Passing By ref
+
+            //Console.WriteLine($"A: {A}");
+            //Console.WriteLine($"B: {B}");
+
+            //double L = 1.5, K = 2.6;
+
+            //Console.WriteLine($"L: {L}");
+            //Console.WriteLine($"K: {K}");
+            //Console.WriteLine(" ********* After SWAP ********* ");
+
+            //Helper.SWAP(ref L, ref K);
+
+            //Console.WriteLine($"L: {L}");
+            //Console.WriteLine($"K: {K}");
+
+            //Point: X, Y
+
+            //Point P01 = new Point(1, 1);
+            //Point P02 = new Point(2, 2);
+
+            //Console.WriteLine($"P01: {P01}");
+            //Console.WriteLine($"P02: {P02}");
+
+            //Console.WriteLine(" ********* After SWAP ********* ");
+
+            //Helper/*<Point>*/.SWAP(ref P01, ref P02);
+
+            //Console.WriteLine($"P01: {P01}");
+            //Console.WriteLine($"P02: {P02}");
         }
     }
 }

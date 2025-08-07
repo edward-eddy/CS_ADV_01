d219409767f39fe3804d9b15b1caeaaba0953ef0
edwar <edwardsameh99@gmail.com>
2025-08-07 15:58:17 +0300

    Q01

---
+++ Assignment/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Q01.
+            /*
+                1. create a generic Range<T> class that represents a range of values from a minimum value to a maximum
+            value. The range should support basic operations such as checking if a value is within the range and 
+            determining the length of the range.
+            */
+            //Range<int> intRange = new Range<int>(7, 10);
+            //Console.WriteLine(intRange.IsInRange(8));
+            //Console.WriteLine(intRange.IsInRange(100));
+
+            //Console.WriteLine();
+
+            //Console.WriteLine($"Length : {intRange.GetLength()}");
+
+            //Console.WriteLine("\n ================== \n");
+
+            //Range<string> stringRange = new Range<string>("7", "10");
+            //Console.WriteLine(stringRange.IsInRange("8"));
+            //Console.WriteLine(stringRange.IsInRange("100"));
+
+            //Console.WriteLine();
+
+            //Console.WriteLine($"Length : {stringRange.GetLength()}"); 
+            #endregion
+
+        }
+    }
+}
